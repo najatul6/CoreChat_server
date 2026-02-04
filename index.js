38f4089084e039be028b02ad84ccb9bebaed2a4c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-02-05 02:34:58 +0600

    feat: update CORS settings to allow multiple origins

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://core-chat-pi.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
